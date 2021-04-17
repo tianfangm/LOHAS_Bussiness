6a66ec3c2618e53cd5f5457ee399239656a2c88c
马天放的PC\matianfang <tianfangma@foxmail.com>
2021-04-17 16:35:45 +0800

    商铺信息修改，暂不支持头像、背景、地理位置修改

--- LOHAS/pages/store/store.js
+++ LOHAS/pages/store/store.js
@@ -5,27 +5,67 @@ Page({
    * 页面的初始数据
    */
   data: {
+    latitude:"",
+    longitude:"",
+    userinfo:[],
+    scale:14,
+    markers:[]
+  },
 
-  },
-  // 返回参数
-  QueryParams:{
-    "avatar": "",
-    "head_picture": "",
-    "shop_address": "",
-    "shop_business_hours": "",
-    "shop_id": 0,
-    "shop_intro": "",
-    "shop_latitude": 0,
-    "shop_lohas_info": "",
-    "shop_longitude": 0,
-    "shop_name": "",
-    "shop_type": ""
-  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    const userinfo = wx.getStorageSync('userInfo');
+    var that = this;
+    that.setData({
+      userinfo:userinfo
+    });
+    this.setMarkers();
+    wx.getLocation({
+      type: 'wgs84', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
+      success: function (res) {
+        that.mpCtx = wx.createMapContext("myMap");
+        //赋值经纬度
+        // console.log("onLoad", res),
+        that.setData({
+          latitude: res.latitude,
+          longitude: res.longitude,
+        })
+      }
+    })
+  },
+
+  setMarkers(){
+    var temp_markers = [];
+    temp_markers.push({
+      id: 0,
+      iconPath: "../../images/map.png",
+      latitude: this.data.userinfo.shop_latitude,
+      longitude: this.data.userinfo.shop_longitude,
+      width: 28,
+      height: 32,
+      desc: this.data.userinfo.shop_id, //这里才是shop_id
+      label: {
+        content: this.data.userinfo.shop_name
+      }
+    });
+    this.setData({
+      markers:temp_markers
+    })
+    console.log(this.data.markers)
+  },
+
+  bindtap(e) {
+    console.log("bindtap", e);
+    // console.log(this.data.latitude, this.data.longitude)
+    this.mpCtx.moveToLocation({
+      longitude:this.data.userinfo.shop_longitude,
+      latitude:this.data.userinfo.shop_latitude,
+      success:()=>{
+        console.log("移过去了")
+      }
+    });
   },
 
   /**
--- LOHAS/pages/user/update.js
+++ LOHAS/pages/user/update.js
@@ -1,18 +1,125 @@
 // pages/user/update.js
+import{request} from "../../request/index.js";
+import regeneratorRuntime from "../../lib/runtime/runtime";
+import Toast from '../../dist/toast/toast';
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    show_type:false,
+    uesrinfo: [],
+    submit_btn_text:"修改",
+    columns: ['衣', '食', '住', '行'],
+    default_type:0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    const userinfo = wx.getStorageSync('userInfo');
+    this.setData({
+      userinfo: userinfo
+    })
+    this.setDefaultType();
+  },
 
+  async updateInfo(){
+    const res = await request({
+      url:"/shopinfo/update",
+      method:"POST",
+      data:this.data.userinfo,
+      header:{
+        "content-type":"application/json",
+        "token":wx.getStorageSync('token')
+      }
+    });
+    if(res.data.state){
+      console.log("修改成功！！！");
+      wx.removeStorageSync('userInfo');
+      wx.setStorageSync('userInfo', this.data.userinfo)
+      wx.navigateBack({
+        delta: 1,
+      })
+    }
+  },
+
+  showPopup() {
+    this.setData({ show_type: true });
+  },
+
+  onClose() {
+    this.setData({ show_type: false });
+  },
+
+  onConfirm(event) {
+    const { picker, value, index } = event.detail;
+    Toast(`当前值：${value}, 当前索引：${index}`);
+    var type = "";
+    switch(index){
+      case 0:
+        type="clothing";
+        break;
+      case 1:
+        type="food";
+        break;
+      case 2:
+        type="housing";
+        break;
+      case 3:
+        type="transportation";
+        break;
+      default:
+        // 错误
+        break;
+    }
+    this.data.userinfo.shop_type = type;
+  },
+
+  onCancel() {
+    Toast('取消');
+  },
+
+  onChangeShopName(event){
+    this.data.userinfo.shop_name=event.detail;
+  },
+
+  onCHangeShopIntro(event){
+    this.data.uesrinfo.shop_intro=event.detail;
+  },
+
+  onChangeShopLOHASInfo(event){
+    this.data.userinfo.shop_lohas_info=event.detail;
+  },
+
+  onChangeShopAddress(event){
+    this.data.userinfo.shop_address=event.detail;
+  },
+
+  onChangeShopBusinessTime(event){
+    this.data.userinfo.shop_business_hours=event.detail;
+  },
+
+  setDefaultType(){
+    var index=0;
+    if(this.data.userinfo.shop_type==='clothing'){
+      index = 0;
+    }
+    else if(this.data.userinfo.shop_type==='food'){
+      index = 1;
+    }
+    else if(this.data.userinfo.shop_type==='housing'){
+      index = 2;
+    }
+    else if(this.data.userinfo.shop_type==='transportation'){
+      index = 3;
+    }
+    this.setData({
+      default_type:index
+    })
   },
 
   /**
